757dbf6767818c41a46a2dd8cf5d9d55a4430a49
Bilal Akbar <geo.bilal7@gmail.com>
2024-05-04 22:34:33 +0500

    Read the labels from the csv file

--- app.py
+++ app.py
@@ -4,56 +4,28 @@ from werkzeug.utils import secure_filename
 from keras.models import load_model
 import numpy as np
 from PIL import Image
+import csv
 
 app = Flask(__name__)
 
-# Classes of trafic signs
-classes = { 0:'Speed limit (20km/h)',
-            1:'Speed limit (30km/h)',
-            2:'Speed limit (50km/h)',
-            3:'Speed limit (60km/h)',
-            4:'Speed limit (70km/h)',
-            5:'Speed limit (80km/h)',
-            6:'End of speed limit (80km/h)',
-            7:'Speed limit (100km/h)',
-            8:'Speed limit (120km/h)',
-            9:'No passing',
-            10:'No passing veh over 3.5 tons',
-            11:'Right-of-way at intersection',
-            12:'Priority road',
-            13:'Yield',
-            14:'Stop',
-            15:'No vehicles',
-            16:'Vehicle > 3.5 tons prohibited',
-            17:'No entry',
-            18:'General caution',
-            19:'Dangerous curve left',
-            20:'Dangerous curve right',
-            21:'Double curve',
-            22:'Bumpy road',
-            23:'Slippery road',
-            24:'Road narrows on the right',
-            25:'Road work',
-            26:'Traffic signals',
-            27:'Pedestrians',
-            28:'Children crossing',
-            29:'Bicycles crossing',
-            30:'Beware of ice/snow',
-            31:'Wild animals crossing',
-            32:'End speed + passing limits',
-            33:'Turn right ahead',
-            34:'Turn left ahead',
-            35:'Ahead only',
-            36:'Go straight or right',
-            37:'Go straight or left',
-            38:'Keep right',
-            39:'Keep left',
-            40:'Roundabout mandatory',
-            41:'End of no passing',
-            42:'End no passing vehicle > 3.5 tons' }
+def load_labels():
+    labels = {}
+    with open('./training/labels.csv', mode='r') as file:
+        reader = csv.reader(file)
+        # Skip the header
+        next(reader)
+        for row in reader:
+            # Assuming the CSV file has two columns: index and label
+            index = int(row[0])
+            label = row[1]
+            labels[index] = label
+    return labels
+
+# Call the function to load labels when the application starts
+classes = load_labels()
 
 def image_processing(img):
-    model = load_model('Traffic_Signs_WebApp/model/TSR.h5')
+    model = load_model('./model/TSR.h5')
     data=[]
     image = Image.open(img).convert('RGB')
     image = image.resize((30,30))
