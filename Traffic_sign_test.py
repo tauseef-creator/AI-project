76b4e08c9b0404b0f40ba2d36a107cb7cac9a2f8
Bilal Akbar <geo.bilal7@gmail.com>
2024-05-05 20:01:30 +0500

    Update the codes and added a new better model

--- Traffic_sign_test.py
+++ Traffic_sign_test.py
@@ -18,7 +18,7 @@ cap.set(3, frameWidth)
 cap.set(4, frameHeight)
 cap.set(10, brightness)
 # IMPORT THE TRANNIED MODEL
-model = load_model('./model/TSR.h5')
+model = load_model('./model/my_model.h5')
 print(model.input_shape)
 
 def grayscale(img):
@@ -28,12 +28,11 @@ def equalize(img):
     img =cv2.equalizeHist(img)
     return img
 def preprocessing(img):
-    img = grayscale(img)
-    img = equalize(img)
-    img = img/255 # Normalize the image
-    img = (img * 255).astype(np.uint8) # Convert back to 8-bit
-    img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # Convert to grayscale
+    img = cv2.equalizeHist(img) # Equalize the histogram
+    img = img / 255 # Normalize the image
     return img
+
 def getCalssName(classNo):
     if   classNo == 0: return 'Speed Limit 20 km/h'
     elif classNo == 1: return 'Speed Limit 30 km/h'
@@ -85,10 +84,10 @@ while True:
 
     # PROCESS IMAGE
     img = np.asarray(imgOrignal)
-    img = cv2.resize(img, (30, 30)) # Resize the image to 30x30 pixels
+    img = cv2.resize(img, (32, 32)) # Resize the image to 30x30 pixels
     img = preprocessing(img)
     cv2.imshow("Processed Image", img)
-    img = img.reshape(1, 30, 30, 3) # Now reshape it to the expected shape
+    img = img.reshape(1, 32, 32, 1) # Now reshape it to the expected shape
     cv2.putText(imgOrignal, "CLASS: " , (20, 35), font, 0.75, (0, 0, 255), 2, cv2.LINE_AA)
     cv2.putText(imgOrignal, "PROBABILITY: ", (20, 75), font, 0.75, (0, 0, 255), 2, cv2.LINE_AA)
     # PREDICT IMAGE
